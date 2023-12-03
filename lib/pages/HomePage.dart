611937344600f9e19af2e58d60d4fccb61cbce7c
kalvin-chakma <kalvinchakma7@gmail.com>
2023-12-04 03:43:44 +0600

    second commit

---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,266 @@
+import 'dart:async';
+
+import 'package:animate_do/animate_do.dart';
+import 'package:assignment/Pages/CategoryPage.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  int activeIndex = 0;
+
+  @override
+  void initState() {
+    Timer.periodic(const Duration(seconds: 30), (timer) {
+      setState(() {
+        activeIndex++;
+
+        if (activeIndex == 3) activeIndex = 0;
+      });
+    });
+
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            FadeInUp(
+              child: Container(
+                height: 450,
+                child: Stack(children: [
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    child: AnimatedOpacity(
+                      opacity: activeIndex == 0 ? 1 : 0,
+                      duration: const Duration(
+                        seconds: 1,
+                      ),
+                      curve: Curves.linear,
+                      child: Image.asset(
+                        "assets/images/b2.png",
+                        height: 400,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    child: AnimatedOpacity(
+                      opacity: activeIndex == 1 ? 1 : 0,
+                      duration: const Duration(seconds: 1),
+                      curve: Curves.linear,
+                      child: Image.asset(
+                        'assets/images/b4.png',
+                        height: 400,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    child: AnimatedOpacity(
+                      opacity: activeIndex == 2 ? 1 : 0,
+                      duration: const Duration(seconds: 1),
+                      curve: Curves.linear,
+                      child: Image.asset(
+                        "assets/images/b3.png",
+                        height: 400,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ]),
+              ),
+            ),
+            FadeInUp(
+                duration: Duration(milliseconds: 1400),
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            "Categories",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text("All")
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        height: 150,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: <Widget>[
+                            makeCategory(
+                                image: 'assets/images/p1.png',
+                                title: 'Magazines',
+                                tag: 'Magazines'),
+                            makeCategory(
+                                image: 'assets/images/p2.png',
+                                title: 'Textbooks',
+                                tag: ''),
+                            makeCategory(
+                                image: 'assets/images/p3.png',
+                                title: 'Notebooks',
+                                tag: 'Notebooks'),
+                            makeCategory(
+                                image: 'assets/images/p4.png',
+                                title: 'Audio Books',
+                                tag: 'Audio Books'),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            "Best Selling by Category",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text("All")
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        height: 150,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: <Widget>[
+                            makeBestCategory(
+                                image: 'assets/images/tech.png', title: 'Tech'),
+                            makeBestCategory(
+                                image: 'assets/images/history.png',
+                                title: 'History'),
+                            makeBestCategory(
+                                image: 'assets/images/newspaper.png',
+                                title: 'Perfume'),
+                            makeBestCategory(
+                                image: 'assets/images/story.png',
+                                title: 'Story'),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 80,
+                      ),
+                    ],
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget makeCategory({image, title, tag}) {
+    return AspectRatio(
+      aspectRatio: 2 / 2.2,
+      child: Hero(
+        tag: tag,
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => CategoryPage(
+                          image: image,
+                          title: title,
+                          tag: tag,
+                        )));
+          },
+          child: Material(
+            child: Container(
+              margin: EdgeInsets.only(right: 20),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                      image: AssetImage(image), fit: BoxFit.cover)),
+              child: Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    gradient:
+                        LinearGradient(begin: Alignment.bottomRight, colors: [
+                      Colors.black.withOpacity(.8),
+                      Colors.black.withOpacity(.0),
+                    ])),
+                child: Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16),
+                    )),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget makeBestCategory({image, title}) {
+    return AspectRatio(
+      aspectRatio: 3 / 2.2,
+      child: Container(
+        margin: EdgeInsets.only(right: 20),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            image:
+                DecorationImage(image: AssetImage(image), fit: BoxFit.cover)),
+        child: Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              gradient: LinearGradient(begin: Alignment.bottomRight, colors: [
+                Colors.black.withOpacity(.8),
+                Colors.black.withOpacity(.0),
+              ])),
+          child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                title,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16),
+              )),
+        ),
+      ),
+    );
+  }
+}
