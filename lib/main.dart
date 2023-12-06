f2940df993202129c6a08704b2a1ba68f4e00040
kalvin-chakma <kalvinchakma7@gmail.com>
2023-12-07 00:07:28 +0600

    Add PAyment Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:assignment/pages/Payment.dart';
 import 'package:assignment/pages/StartPage.dart';
 import 'package:flutter/material.dart';
 
 void main() =>
-    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: StartPage()));
+    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Payment()));
--- lib/pages/Payment.dart
+++ lib/pages/Payment.dart
@@ -1,15 +1,228 @@
 import 'package:flutter/material.dart';
 
 class Payment extends StatefulWidget {
-  const Payment({super.key});
+  const Payment({Key? key}) : super(key: key);
 
   @override
-  State<Payment> createState() => _PaymentState();
+  _PaymentState createState() => _PaymentState();
 }
 
 class _PaymentState extends State<Payment> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          "Payment",
+          style: TextStyle(color: Colors.black, fontSize: 30),
+        ),
+        leading: BackButton(color: Colors.black),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(children: [
+            Container(
+              height: 200,
+              width: double.infinity,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  gradient: LinearGradient(colors: [
+                    Colors.green,
+                    Colors.green.shade800,
+                    Colors.green.shade900
+                  ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "credit card",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "**** **** **** 7890",
+                        style: TextStyle(color: Colors.white, fontSize: 30),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "username",
+                            style: TextStyle(color: Colors.white, fontSize: 30),
+                          ),
+                          // Image.network(
+                          //     'https://img.icons8.com/color/2x/mastercard-logo.png',
+                          //     height: 50),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 120,
+              width: double.infinity,
+              padding: EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Payment Method",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                        onTap: () {},
+                        child: AnimatedContainer(
+                          duration: Duration(milliseconds: 300),
+                          margin: EdgeInsets.only(right: 10),
+                          padding: EdgeInsets.symmetric(horizontal: 20),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          child: Image(
+                            image: AssetImage("assets/images/p1.png"),
+                            height: 30,
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.red,
+                          ),
+                          child: Image(
+                            image: AssetImage("assets/images/p1.png"),
+                            height: 30,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Column(
+              children: [
+                Container(
+                  height: 50,
+                  padding: EdgeInsets.only(
+                    left: 20,
+                  ),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Offers",
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w600),
+                      ),
+                      TextButton(onPressed: () {}, child: Text("Add a code"))
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 50,
+              padding: EdgeInsets.only(
+                left: 20,
+              ),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10), color: Colors.white),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Address",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
+                  ),
+                  TextButton(
+                      onPressed: () {},
+                      child: Row(
+                        children: [
+                          Text("xxxxxxxxxxxxxxxxxx"),
+                          Icon(
+                            Icons.keyboard_arrow_down,
+                            size: 20,
+                          )
+                        ],
+                      ))
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Total Payment",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                ),
+                Text("\৳000.00",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))
+              ],
+            ),
+            SizedBox(height: 30),
+            MaterialButton(
+              onPressed: () {},
+              height: 50,
+              elevation: 0,
+              splashColor: Colors.yellow[700],
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              color: Colors.yellow[800],
+              child: Center(
+                  child: Container(
+                child: Text(
+                  "Pay Now",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              )),
+            ),
+          ]),
+        ),
+      ),
+    );
   }
 }
